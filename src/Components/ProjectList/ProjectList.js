d6ff1640991dc83836029a122735ef6c84d6a52e
shoorano <56363369+shoorano@users.noreply.github.com>
2020-07-08 23:28:29 +0100

    Debugging.

--- src/Components/ProjectList/ProjectList.js
+++ src/Components/ProjectList/ProjectList.js
@@ -35,14 +35,14 @@ const ProjectList = () => {
             className=""
             name='Quote Generator'
             link='https://shoorano.github.io/quote-generator/'
-            image='quote.jpg'
+            image='src/Components/Projects/quote.jpg'
             desc='Quote generator which fetches from an API and uses DOM manipulation to update, and or tweet the quote.'
           />
           <Projects
             className=""
             name='Infinite Scroll'
             link='https://shoorano.github.io/infinite-scroll/'
-            image='scroll.jpg'
+            image='src/Components/Projects/scroll.jpg'
             desc='JS app which continually updates images pulled from Unsplash API as the user scrolls.'
           />
       </div>
