38784f010c37f984c2bd1eac78749ef2282d9d48
shoorano <56363369+shoorano@users.noreply.github.com>
2020-07-18 18:05:22 +0100

    Added latest version of shoorano-clothing to projects list.

--- src/Components/ProjectList/ProjectList.js
+++ src/Components/ProjectList/ProjectList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Projects from "../Projects/Projects.js";
 import quote from "./quote.jpg";
 import scroll from "./scroll.jpg";
+import clothes from './clothes.png'
 
 const h = {
    color: "Black",
@@ -22,6 +23,13 @@ const ProjectList = () => {
       <div className="center" style={div}>
           <Projects
             className=""
+            name='shoorano-clothing'
+            link='https://shoorano-clothing.herokuapp.com'
+            image={ clothes }
+            desc='E-Commerce site built using react-redux for the frontend and firebase as a backend. Site has google login and stripe payments integrated.'
+          />
+          <Projects
+            className=""
             name='nst-art'
             link='https://nst-art.herokuapp.com'
             image='https://www.americanscientist.org/sites/americanscientist.org/files/2019-107-1-18-elgammal-2-natural.jpg'
@@ -38,14 +46,14 @@ const ProjectList = () => {
             className=""
             name='Quote Generator'
             link='https://shoorano.github.io/quote-generator/'
-            image={quote}
+            image={ quote }
             desc='Quote generator which fetches from an API and uses DOM manipulation to update, and or tweet the quote.'
           />
           <Projects
             className=""
             name='Infinite Scroll'
             link='https://shoorano.github.io/infinite-scroll/'
-            image={scroll}
+            image={ scroll }
             desc='JS app which continually updates images pulled from Unsplash API as the user scrolls.'
           />
       </div>
